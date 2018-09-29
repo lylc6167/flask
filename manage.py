3bf346e273d3b2b428e07c5fdac41e03fa875b2a
youluTec__6167 <6167.good@163.com>
2018-09-29 15:14:24 +0800

    test .gitignore

--- manage.py
+++ manage.py
@@ -2,13 +2,18 @@
 # 上述代码为https://blog.csdn.net/jmilk/article/details/53152158
 # 文章中所填代码，和实际误差有点大
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
+
 import main
 import models
 
 
 manager = Manager(main.app)
+migrate = Migrate(main.app, models.db)
 
 manager.add_command("server", Server())
+manager.add_command("db", MigrateCommand)
 
 
 @manager.shell
@@ -17,7 +22,8 @@ def make_shell_context():
         app=main.app,
         db=models.db,
         User=models.User,
-        Post=models.Post)
+        Post=models.Post,
+        Comment=models.Comment)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -6,10 +6,12 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+
     __tablename__ = 'users'
     id = db.Column(db.String(45), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+
     posts = db.relationship(
         'Post',
         backref='users',
@@ -24,16 +26,63 @@ class User(db.Model):
         return "<Model User '{}'>".format(self.username)
 
 
+posts_tags = db.Table('posts_tags',
+    db.Column('post_id', db.String(45), db.ForeignKey('posts.id')),
+    db.Column('tag_id', db.String(45), db.ForeignKey('tags.id')))
+
+
 class Post(db.Model):
+
     __tablename__ = 'posts'
     id = db.Column(db.String(45), primary_key=True)
     title = db.Column(db.String(355))
     text = db.Column(db.Text())
     created_date = db.Column(db.DateTime)
+
     user_id = db.Column(db.String(45), db.ForeignKey('users.id'))
+
+    comments = db.relationship(
+        'Comment',
+        backref='posts',
+        lazy='dynamic')
+
+    tags = db.relationship(
+        'Tag',
+        secondary='posts_tags',
+        backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, title):
         self.title = title
 
     def __repr__(self):  # 命令行修改语句
         return "<Model Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+
+    __tabelname__ = 'comments'
+    id = db.Column(db.String(45), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    created_time = db.Column(db.DateTime())
+
+    post_id = db.Column(db.String(45), db.ForeignKey('posts.id'))
+
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Model Comment '{}'>".format(self.name)
+
+
+class Tag(db.Model):
+
+    __tablename__ = 'tags'
+    id = db.Column(db.String(45), primary_key=True)
+    name = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Model Tag '{}'>".format(self.name)
